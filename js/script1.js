4a3126dd8dd35f93dc21c3cb93d3093ab7cfe1e8
yoshio-manbe <whs.hu.sy33@gmail.com>
2023-05-14 21:23:29 +0900

    ID動作変更

--- js/script1.js
+++ js/script1.js
@@ -15,37 +15,11 @@ $(function(){
     
     //都道府県をクリック
     $('.pref_list [data-id]').click(function(){
-        if($(this).data('id')){
-            var id = $(this).data('id');
-            if(id >= 1 && id <= 7){
-                $(".A1").html("北海道")
-                // 要素を取得する
-                const element = document.querySelector('.A1');
-                // hiddenクラスを削除する
-                element.classList.remove('A1');
-
-                // window.location.href = "index2.html";
-            } else if(id >= 8 && id <= 14){
-                window.location.href = "index3.html"
-            } else if(id >= 15 && id <= 23){
-                window.location.href = "index4.html"
-            } else if(id >= 24 && id <= 30){
-                window.location.href = "index2.html"
-            } else if(id >= 31 && id <= 39){
-                window.location.href = "index3.html"
-            } else if(id >= 40 && id <= 47){
-                window.location.href = "index4.html"
-            } 
-            
-            
-            
-            
-            
-            prefReset();
-        }
-        
-    });
-    
+        var id = $(this).data('id');
+        var text = $(this).text();
+        console.log(text); 
+        $('.A1').html(text);
+        })
     
     //表示リセット
     function prefReset(){
